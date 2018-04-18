9395191105c68569abe9b578198ce96b1c9298fe
evaseemefly-surfacebook <evaseemefly@126.com>
2018-04-19 06:09:35 +0800

    使用装饰器模式创建单利模式

--- demo_3.py
+++ demo_3.py
@@ -1,19 +1,43 @@
 
+# 使用pythons实现单例模式
+# 方式1：
 class SingleTon(object):
     def __new__(cls, *args, **kwargs):
         if not hasattr(cls,'_instance'):
             cls._instance=object.__new__(cls,*args,**kwargs)
         return cls._instance
 
+
 class TestClass(SingleTon):
-    a=1
-    name="小明"
+    a = 1
+    name = "小明"
+    
     def sayhi(self):
-        msg="你好我是%s"%(self.name)
+        msg = "你好我是%s" % (self.name)
         return msg
 
-test1=TestClass()
-test2=TestClass()
+# 方式2：使用装饰器
+def SingleTon_Dec(cls,*args,**kwargs):
+    instances={}
+    # 装饰器中里面的方法起任何名字均可以吗？
+    def _singleton():
+        if cls not in instances:
+            instances[cls]=cls(*args,**kwargs)
+        return instances[cls]
+    return _singleton
+
+@SingleTon_Dec
+class TestClass_Dec(object):
+    a = 1
+    name = "小明"
+    
+    def sayhi(self):
+        msg = "你好我是%s" % (self.name)
+        return msg
+    
+
+test1=TestClass_Dec()
+test2=TestClass_Dec()
 print(test1)
 print(test2)
 
